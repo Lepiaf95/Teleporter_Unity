173cc100b6f7188f61017ea8eb90cbc1859f2138
Geoffrey <45283124+Lepiaf95@users.noreply.github.com>
2019-02-12 16:26:57 +0100

    Add files via upload

---
+++ Teleport.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    private Transform posStart;
+
+    private void Start()
+    {
+        posStart = GameObject.Find("spawn").GetComponent<Transform>();
+        //print(posStart.position);
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Teleporter")
+            this.transform.position = posStart.position;
+        //Time.timeScale = 0.1f;
+    }
+}
