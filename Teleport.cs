4bcdd44824357cdbca5f597cc4c48717698f79d7
Geoffrey <45283124+Lepiaf95@users.noreply.github.com>
2019-02-13 00:11:04 +0100

    Add files via upload

--- Teleport.cs
+++ Teleport.cs
@@ -6,17 +6,39 @@ public class Teleport : MonoBehaviour
 {
     private Transform posStart;
 
+    public Player m_player; // Le FPSController
+
+
     private void Start()
     {
         posStart = GameObject.Find("spawn").GetComponent<Transform>();
-        //print(posStart.position);
+        this.transform.position = posStart.position;
     }
-
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Teleporter")
-            this.transform.position = posStart.position;
-        //Time.timeScale = 0.1f;
+        {
+            StartCoroutine(teleportation(0.1f)); // 0.1f afin de laisser le temps aux teleporteurs de faire leurs propres effets
+        }
+
+        if (other.gameObject.tag == "Finish")
+        {
+            StartCoroutine(teleportation(2.0f));
+        }
+    }
+
+    IEnumerator teleportation(float x)
+    {
+        yield return new WaitForSeconds(x);
+        this.transform.position = posStart.position;
+        m_player.enabled = false; // Script désActivé le temps d'enregistrer la nouvelle position du player entre 2 frames
+        StartCoroutine(moveAgain());
+    }
+
+    IEnumerator moveAgain()
+    {
+        yield return new WaitForSeconds(0.3f);
+        m_player.enabled = true; // Script reActivé
     }
 }
