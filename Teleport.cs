04b565aed588987ef013cb8114f1d7783849c07f
Geoffrey <45283124+Lepiaf95@users.noreply.github.com>
2019-02-13 10:27:41 +0100

    Add files via upload

--- Teleport.cs
+++ Teleport.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityStandardAssets.Characters.FirstPerson; // Permet d'utiliser le type FirstPersonController - asset d'Unity
+
 public class Teleport : MonoBehaviour
 {
     private Transform posStart;
 
-    public Player m_player; // Le script du FPSController
-
+    public FirstPersonController m_player; // Le FPSController d'Unity
 
     private void Start()
     {
@@ -24,7 +25,7 @@ public class Teleport : MonoBehaviour
 
         if (other.gameObject.tag == "Finish")
         {
-            StartCoroutine(teleportation(2.0f));
+            FindObjectOfType<GameManager>().gameWin();
         }
     }
 
@@ -33,12 +34,11 @@ public class Teleport : MonoBehaviour
         yield return new WaitForSeconds(x);
         this.transform.position = posStart.position;
         m_player.enabled = false; // Script désActivé le temps d'enregistrer la nouvelle position du player entre 2 frames
-        StartCoroutine(moveAgain());
+        Invoke("moveAgain", 0.3f);
     }
 
-    IEnumerator moveAgain()
+    private void  moveAgain()
     {
-        yield return new WaitForSeconds(0.3f);
         m_player.enabled = true; // Script reActivé
     }
 }
